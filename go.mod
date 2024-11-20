dae9982592761a83990864587d64342f4a7e9854
MSKU786 <manishsingh332000@gmail.com>
2024-11-20 11:48:54 +0530

    Setting up main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module restaurant-managment-system
+
+go 1.23.2
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"os"
+	"restaurant-managment-system/database"
+	"restaurant-managment-system/middleware"
+	"restaurant-managment-system/routes"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+var foodCollection *mongo.Collection  = database.OpenCollection(database.Client, "food")
 func main () {
-	
+		port := os.Getenv("PORT");
+
+		if port == "" {
+			port = "8800"
+		}
+ 
+		router := gin.New();
+		routes.Use(gin.Logger());
+		routes.UseRoutes(router)
+		router.Use(middleware.Authentication())
+
+
+		routes.FoodRoutes(router);
+		routes.MenuRoutes(router);
+		routes.TableRoutes(router);
+		routes.OrderRoutes(router);
+		routes.OrderItemRoutes(router);
+		routes.UserRoutes(router);
+		routes.InvoiceRoutes(router);
+		 
+		router.Run(":" + port);
 }
