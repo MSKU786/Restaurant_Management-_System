6f443651ee58852ea559efe0aedd2f1bb4fea47d
MSKU786 <manishsingh332000@gmail.com>
2024-11-21 10:54:13 +0530

    Created food router

--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -7,7 +7,7 @@ import (
 
 func FoodRoutes(incomingRoutes *gin.Engine) {
 	 incomingRoutes.GET("/foods", controller.GetFoods())
-	 incomingRoutes.GET("/users/:user_id", controller.GetUser())
-	 incomingRoutes.POST("/users/singup", controller.SignUp())
-	 incomingRoutes.POST("/users/login", controller.Login())
+	 incomingRoutes.GET("/foods/:food_id", controller.GetFood())
+	 incomingRoutes.POST("/foods", controller.CreateFood())
+	 incomingRoutes.PATCH("/foods/:food_id", controller.UpdateFood())
 }
