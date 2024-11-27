6c2c8f951be7f29eae246d4b127bbeca94e8a308
MSKU786 <manishsingh332000@gmail.com>
2024-11-27 12:05:59 +0530

    update menu function completed

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
@@ -95,6 +96,62 @@ func CreateMenu() gin.HandlerFunc{
 
 func UpdateMenu() gin.HandlerFunc{
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
 
+		if err := c.BindJSON(&menu); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return 
+		}
+
+		menuId := c.Param("menu_id")
+		filter := bson.M{"menu_id":menuId}
+
+		var updatedObj primitive.D
+
+		if menu.Start_date != nil && menu.End_date != nil {
+			if !inTimeSpan(*menu.Start_date, *menu.End_date, time.Now()) {
+				msg := "Kindly retype the time"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				defer cancel();
+				return;
+			}
+
+			updatedObj = append(updatedObj, bson.E{Key: "start_date", Value: menu.Start_date});
+			updatedObj = append(updatedObj, bson.E{Key: "end_date", Value: menu.End_date})
+
+			if menu.Name != "" {
+				updatedObj = append(updatedObj, bson.E{Key: "name", Value: menu.Name})
+			}
+			if menu.Category != "" {
+				updatedObj = append(updatedObj, bson.E{Key: "category", Value: menu.Category})
+			}
+
+			menu.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			updatedObj = append(updatedObj, bson.E{Key: "updated_at", Value: menu.Updated_at})
+
+			upsert := true
+
+			opt := options.UpdateOptions{
+				Upsert : &upsert,
+			}
+
+			result, err := menuCollection.UpdateOne(
+				ctx, 
+				filter,
+				bson.D{
+					{Key: "$set", Value: updatedObj},
+				},
+				&opt,
+			)
+			if err !=nil {
+				msg := fmt.Sprintf("Menu update failed")
+				c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+				return;
+			}
+
+			defer cancel()
+			c.JSON( http.StatusOK, result);
+		}
 	}
 }
