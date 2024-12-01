1ce0cd8d3de774198b23f3086e24694d8d9d2c2b
MSKU786 <manishsingh332000@gmail.com>
2024-12-01 12:07:06 +0530

    get order completed

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,9 +1,40 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"restaurant-managment-system/database"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
+
 
 func GetOrders() gin.HandlerFunc{
 		return func(c *gin.Context) {
+			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+			result , err := orderCollection.Find(context.TODO(), bson.M{});
+			defer cancel();
+
+			if (err != nil) {
+				c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing orders"})
+			}
+
+			var allOrders []bson.M;
+
+			if err = result.All(ctx, &allOrders) ; err != nil {
+				log.Fatal(err);
+			}
+
+			c.JSON(http.StatusOK, allOrders);
 
 		}
 }
