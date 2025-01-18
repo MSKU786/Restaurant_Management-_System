d85c706fa920631a96f1131c54bb6dba1e03ea95
MSKU786 <manishsingh332000@gmail.com>
2025-01-18 11:20:11 +0530

    Added orderItemControoler getOrderItems function

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,13 +1,49 @@
 package controller
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"restaurant-managment-system/database"
+	"restaurant-managment-system/models"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+
+type OrderItemPack struct {
+	Table_id *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem");
+
+
 
 func GetOrderItems() gin.HandlerFunc{
 		return func(c *gin.Context) {
+				var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second);
+				
+				result, err := orderItemCollection.Find(context.TODO(), bson.M{});
+				defer cancel();	
 
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing order items"})
+					return
+				}	
+
+				var allOrderItems []bson.M;
+
+				if err = result.All(ctx, &allOrderItems) ; err != nil {
+					log.Fatal(err);
+					return
+				}	
+
+				c.JSON(http.StatusOK, allOrderItems);
 		}
 }
 
