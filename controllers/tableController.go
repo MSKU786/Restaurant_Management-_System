fb6de9bb5fc83590a6e5a1369e29db268d105fc5
MSKU786 <manishsingh332000@gmail.com>
2025-01-23 12:07:36 +0530

    Rsovled few bugs and started with table controller

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,10 +1,35 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"restaurant-managment-system/database"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table");
 
 func GetTables() gin.HandlerFunc{
 		return func(c *gin.Context) {
+				ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second );
 
+				result, err := tableCollection.Find(context.TODO(), bson.M{});
+				defer cancel()();
+
+				if (err != nil) {
+					c.JSON(https.StatusInternalServerError, gin.H{"error": "error occured while fetching the tables"})
+				}
+
+				var allTables []bson.M;
+
+				if err = result.All(ctx, &allTables) : err != nil {
+					log.Fatal(err);
+				}
+				c.JSON(http.StatusOK, allTables);
 		}
 }
 
