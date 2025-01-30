c81fe50da38c97627c7424053a2287a33c36e755
MSKU786 <manishsingh332000@gmail.com>
2025-01-30 11:18:31 +0530

    Fixed table controller bugs

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"net/http"
 	"restaurant-managment-system/database"
+	"restaurant-managment-system/models"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,15 +23,15 @@ func GetTables() gin.HandlerFunc{
 				ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second );
 
 				result, err := tableCollection.Find(context.TODO(), bson.M{});
-				defer cancel()();
+				defer cancel();
 
 				if (err != nil) {
-					c.JSON(https.StatusInternalServerError, gin.H{"error": "error occured while fetching the tables"})
+					c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the tables"})
 				}
 
 				var allTables []bson.M;
 
-				if err = result.All(ctx, &allTables) : err != nil {
+				if err = result.All(ctx, &allTables); err != nil {
 					log.Fatal(err);
 				}
 				c.JSON(http.StatusOK, allTables);
@@ -41,7 +43,7 @@ func GetTable() gin.HandlerFunc{
 			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second);
 			tableId := c.Param("table_id");
 
-			var table model.Table;
+			var table models.Table;
 
 			err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table);
 			defer cancel()
@@ -57,7 +59,7 @@ func GetTable() gin.HandlerFunc{
 func CreateTable() gin.HandlerFunc{
 	return func(c *gin.Context) {
 			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second);	
-			var table model.Table;
+			var table models.Table;
 
 			if err := c.BindJSON(&table); err != nil{
 				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -71,21 +73,21 @@ func CreateTable() gin.HandlerFunc{
 				return;
 			}
 
-			table.created_at , _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339));
-			table.updated.at , _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339));
+			table.Created_at , _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339));
+			table.Updated_at , _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339));
 			table.ID = primitive.NewObjectID();
 			table.Table_id = table.ID.Hex();
 
 			result, insertErr := tableCollection.InsertOne(ctx, table);
 
 			if insertErr != nil {
-				msg: fmt.Sprintf("Table item was not created")
+				msg := fmt.Sprintf("Table item was not created")
 				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 				return;
 			}
 
 			defer cancel();
-			c.JSON(http.StatusOK, table);
+			c.JSON(http.StatusOK, result);
 
 
 	}
@@ -95,26 +97,26 @@ func UpdateTable() gin.HandlerFunc{
 	return func(c *gin.Context) {
 			ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
-			var table model.Table;
+			var table models.Table;
 
-			tableId = c.Param("Table_id");
+			tableId := c.Param("Table_id");
 
 			if err := c.BindJSON(&table); err != nil {
-				c.JSON(https.StatusBadRequest, gin.H{"error": err.Error()});
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()});
 				return;
 			}
 
 			var updateObj primitive.D;
 
 			if table.Number_of_guests != nil {
-				updateObj = append(updateObj, bson.E{"number_of_guests", table.Number_of_guests})
+				updateObj = append(updateObj, bson.E{Key: "number_of_guests", Value: table.Number_of_guests})
 			}
 
 			if table.Table_number != nil {
-				updateObj = append(updateObj, bson.E{"table_number", table.Table_number})
+				updateObj = append(updateObj, bson.E{Key: "table_number", Value: table.Table_number})
 			}
 
-			table.updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339));
+			table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339));
 			upsert := true;
 
 			opt := options.UpdateOptions{
@@ -122,16 +124,16 @@ func UpdateTable() gin.HandlerFunc{
 			}
 
 			filter := bson.M{"table_id": tableId};
-			tableCollection.UpdateOne(
+			_ , err := tableCollection.UpdateOne(
 				ctx,
 				filter, 
 				bson.D{{Key: "$set", Value: updateObj}},
-				&opt
+				&opt,
 			)
 
 			if err != nil {
 				msg := fmt.Sprintf("table item update failed")
-				c.JSON(http.StatueInternalServerError, gin.H{"error": msg})
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			}
 
 			defer cancel();
