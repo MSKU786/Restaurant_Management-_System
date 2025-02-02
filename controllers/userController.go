28bc798cbcc7ed126fa183b8a9551e7c60d892a0
MSKU786 <manishsingh332000@gmail.com>
2025-02-02 07:23:54 +0530

    Added getUSers functions with simple feching all users

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,39 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"restaurant-managment-system/database"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user");
+
 
 func GetUsers() gin.HandlerFunc{
 		return func(c *gin.Context) {
+				var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second);
 
+				var allUsers []bson.M;
+
+				result ,err := userCollection.Find(context.TODO(), bson.M{})
+
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error":"Error occured while listing users"})
+					return;
+				}
+
+
+				if err = result.All(ctx, &allUsers); err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while decoding users"});
+					return;
+				}
+
+				c.JSON(http.StatusOK, allUsers);
 		}
 }
 
