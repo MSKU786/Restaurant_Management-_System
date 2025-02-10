cd0d0ee6faaad1e730adbbb4c4688fc439655bde
MSKU786 <manishsingh332000@gmail.com>
2025-02-10 11:35:24 +0530

    Started with tokenHelper functions

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,7 +1,25 @@
-package helpers 
+package helpers
 
-import ()
+import (
+	"os"
+	"restaurant-managment-system/database"
 
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v4"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SingerDetails struct {
+	Email string
+	First_name string
+	Last_name string
+	Uid string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
 func GenerateAllTokens() {
 
 }
